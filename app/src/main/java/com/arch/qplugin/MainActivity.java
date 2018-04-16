e6385acf97df52bcbfadd0fedd5797bc1f7a72e5
wurongqiu <wurongqiu@kaike.la>
2018-04-16 17:58:13 +0800

    plugin - init  main

--- app/src/main/java/com/arch/qplugin/MainActivity.java
+++ app/src/main/java/com/arch/qplugin/MainActivity.java
@@ -1,13 +1,106 @@
 package com.arch.qplugin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    public static final String FROM = "extra.from";
+    public static final int FROM_INTERNAL = 0;
+    public static final int FROM_EXTERNAL = 1;
+
+    private ArrayList<PluginItem> mPluginItems = new ArrayList<PluginItem>();
+    private PluginAdapter mPluginAdapter;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private class PluginAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        public PluginAdapter() {
+            mInflater = MainActivity.this.getLayoutInflater();
+        }
+
+        @Override
+        public int getCount() {
+            return mPluginItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mPluginItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.plugin_item, parent, false);
+                holder = new ViewHolder();
+                holder.appIcon = (ImageView) convertView.findViewById(R.id.app_icon);
+                holder.appName = (TextView) convertView.findViewById(R.id.app_name);
+                holder.apkName = (TextView) convertView.findViewById(R.id.apk_name);
+                holder.packageName = (TextView) convertView.findViewById(R.id.package_name);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            PluginItem item = mPluginItems.get(position);
+            PackageInfo packageInfo = item.packageInfo;
+//            holder.appIcon.setImageDrawable(DLUtils.getAppIcon(MainActivity.this, item.pluginPath));
+//            holder.appName.setText(DLUtils.getAppLabel(MainActivity.this, item.pluginPath));
+            holder.apkName.setText(item.pluginPath.substring(item.pluginPath.lastIndexOf(File.separatorChar) + 1));
+            holder.packageName.setText(packageInfo.applicationInfo.packageName);
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+        public ImageView appIcon;
+        public TextView appName;
+        public TextView apkName;
+        public TextView packageName;
+    }
+
+    public static class PluginItem {
+        public PackageInfo packageInfo;
+        public String pluginPath;
+
+        public PluginItem() {
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        PluginItem item = mPluginItems.get(position);
+//        DLPluginManager pluginManager = DLPluginManager.getInstance(this);
+//        pluginManager.startPluginActivity(this, new DLIntent(item.packageInfo.packageName));
+
+    }
+
+
 }
